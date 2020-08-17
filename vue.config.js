9c3c7793c6366072425b5ee084aec9f88a04f0aa
hezhaoshun <373693643@qq.com>
2020-08-17 14:22:09 +0800

    px2rem-loader + lib-flexible 实现自适应

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('css')
+        .test(/\.css$/)
+        .oneOf('vue')
+        .resourceQuery(/\?vue/)
+        .use('px2rem')
+          .loader('px2rem-loader')
+          .options({
+            remUnit: 75
+          });
+  },
+  devServer: {
+    port: 6060,
+  },
+}
